8f562a8a86305d5ff42c3f58f5209f62928d0d4b
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-07-11 16:46:34 +0330

    about page created

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,33 @@
 // swiper
 
-var months = new Swiper(".months", {
+document.querySelectorAll('.installment-slider').forEach(sliderContainer => {
+  const swiperEl = sliderContainer.querySelector('.swiper');
+  const contentContainer = sliderContainer.querySelector('.installment-content');
+  
+  new Swiper(swiperEl, {
     slidesPerView: 1,
     spaceBetween: 20,
     loop: true,
     navigation: {
-        nextEl: '.swiper-button-next', // دکمه بعدی
-        prevEl: '.swiper-button-prev', // دکمه قبلی
+      nextEl: swiperEl.querySelector('.swiper-button-next'),
+      prevEl: swiperEl.querySelector('.swiper-button-prev'),
     },
+    on: {
+      init: function() {
+        const firstSlide = this.slides[0];
+        const firstDataId = firstSlide.getAttribute('data-1d');
+        contentContainer.querySelector(`.monthcontent[data-1d="${firstDataId}"]`).classList.add('active');
+      },
+      slideChange: function() {
+        const activeSlide = this.slides[this.activeIndex];
+        const dataId = activeSlide.getAttribute('data-1d');
+        
+        contentContainer.querySelectorAll('.monthcontent').forEach(content => {
+          content.classList.remove('active');
+        });
+        
+        contentContainer.querySelector(`.monthcontent[data-1d="${dataId}"]`).classList.add('active');
+      }
+    }
+  });
 });
