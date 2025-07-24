d3f0db4aaa5e57657a1d45deae69e2832280a3d5
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-07-24 23:44:34 +0330

    first commit

--- js/chart.js
+++ js/chart.js
@@ -17,16 +17,16 @@ function createPieChart(chartId, values, colors) {
 // مقداردهی اولیه چارت‌ها
 document.addEventListener('DOMContentLoaded', function() {
     // چارت 1
-    createPieChart('pieChart1', [123540, 50000, 50000], ['#005AFE', '#AACDFC', '#598BF6']);
+    createPieChart('pieChart1', [123540, 50000, 50000], ['#da1984', '#E975B5', '#F8D1E6']);
     
     // چارت 2
-    createPieChart('pieChart2', [90000, 60000, 30000], ['#005AFE', '#AACDFC', '#598BF6']);
+    createPieChart('pieChart2', [90000, 60000, 30000], ['#da1984', '#E975B5', '#F8D1E6']);
     
     // چارت 3
-    createPieChart('pieChart3', [90000, 60000, 30000], ['#005AFE', '#AACDFC', '#598BF6']);
+    createPieChart('pieChart3', [90000, 60000, 30000], ['#da1984', '#E975B5', '#F8D1E6']);
     
     // چارت 4
-    createPieChart('pieChart4', [90000, 60000, 30000], ['#005AFE', '#AACDFC', '#598BF6']);
+    createPieChart('pieChart4', [90000, 60000, 30000], ['#da1984', '#E975B5', '#F8D1E6']);
     
     // برای اضافه کردن چارت جدید:
     // createPieChart('pieChart3', [value1, value2, value3], ['color1', 'color2', 'color3']);
